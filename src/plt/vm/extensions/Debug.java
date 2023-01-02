e166aa707163f4b118c1d8fc49cd3181ab8b9e49
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-02 17:45:19 +0100

    rework debug print

--- src/plt/vm/extensions/Debug.java
+++ src/plt/vm/extensions/Debug.java
@@ -2,11 +2,24 @@ package plt.vm.extensions;
 
 import plt.vm.Extension;
 
+import java.util.Arrays;
+
 public class Debug extends Extension
 {
     public Debug()
     {
         super("debug");
-        function("print", (c, v) -> System.out.println(c.get(v.inputs()[0])));
+        function("print", (c, v) -> {
+            Object o = c.get(v.inputs()[0]);
+
+            if(o instanceof Object[] a)
+            {
+                System.out.println(Arrays.toString(a));
+            }
+            else
+            {
+                System.out.println(o);
+            }
+        });
     }
 }
