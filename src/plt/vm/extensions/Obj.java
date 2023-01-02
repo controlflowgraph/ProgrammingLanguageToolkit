168ed92896790615186c41564972ec4fae5303f9
ControlFlow Graph <controlflowgraph@gmail.com>
2023-01-02 10:11:32 +0100

    add Obj extension

---
+++ src/plt/vm/extensions/Obj.java
@@ -0,0 +1,71 @@
+package plt.vm.extensions;
+
+import plt.vm.Extension;
+
+import java.util.List;
+
+public class Obj extends Extension
+{
+    public record Descriptor(String name, List<String> fields)
+    {
+
+    }
+
+    public record Instance(Descriptor descriptor, Object[] fields)
+    {
+        public void setField(String name, Object value)
+        {
+            this.fields[getFieldIndex(name)] = value;
+        }
+
+        public <T> T getField(String name, Class<T> cls)
+        {
+            return cls.cast(getField(name));
+        }
+
+        public Object getField(String name)
+        {
+            return this.fields[getFieldIndex(name)];
+        }
+
+        private int getFieldIndex(String name)
+        {
+            List<String> strings = this.descriptor.fields();
+            for (int i = 0; i < strings.size(); i++)
+            {
+                String field = strings.get(i);
+                if (field.equals(name))
+                {
+                    return i;
+                }
+            }
+            throw new RuntimeException("No field with name '" + name + "' on type '" + this.descriptor.name() + "'!");
+        }
+    }
+
+    public Obj()
+    {
+        super("obj");
+
+        function("create", (c, v) -> {
+            Descriptor descriptor = v.data(Descriptor.class);
+            int size = descriptor.fields().size();
+            Object[] elements = new Object[size];
+            c.set(v.output(), new Instance(descriptor, elements));
+        });
+
+        function("get", (c, v) -> {
+            Instance inst = c.get(v.inputs()[0], Instance.class);
+            String field = v.data(String.class);
+            Object value = inst.getField(field);
+            c.set(v.output(), value);
+        });
+
+        function("set", (c, v) -> {
+            Instance inst = c.get(v.inputs()[0], Instance.class);
+            String field = v.data(String.class);
+            Object value = c.get(v.inputs()[1]);
+            inst.setField(field, value);
+        });
+    }
+}
